a6ce2e70c04de9b66357ba8b3b0a682e28d24645
Emily McKinney <emily.a.mckinney@gmail.com>
2022-10-12 12:22:13 -0500

    Ready to start?

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ready to guess the beta?</Text>
       <StatusBar style="auto" />
     </View>
   );
