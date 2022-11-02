29bf7d910fb6a9d628105d2733acd791a4598961
Emily McKinney <emily.a.mckinney@gmail.com>
2022-11-02 13:35:35 -0500

    image picker

--- components/BetaScreen.js
+++ components/BetaScreen.js
@@ -1,22 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React ,{useState} from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import * as ImagePicker from 'expo-image-picker';
 
-class BetaScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Add betas here!</Text>
 
-        <Button
-          title="Go back hooooome"
-          onPress={() =>
-            this.props.navigation.navigate('Home')
-          }
-        />
+export default function BetaScreen() {
+  const [image,setImage] = useState(null)
 
-      </View>
-    );
-  }
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      allowsEditing:true
+    });
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+  
+  
+  
+  return (
+    <View style={styles.container}>
+      <Text>Add betas here!</Text>
+      
+      <StatusBar hidden={true} />
+      {image && <Image source={{uri:image}} style={{flex:1,width:600}} />}
+      <Button title="Pick Image" onPress={pickImage}/>
+      <StatusBar style="auto" />
+
+      <Button
+        title="Go back hooooome"
+        onPress={() =>
+          this.props.navigation.navigate('Home')
+        }
+      />
+
+    </View>
+  );
+  
 }
 
 const styles = StyleSheet.create({
@@ -28,4 +51,3 @@ const styles = StyleSheet.create({
   },
 });
 
-export default BetaScreen;
